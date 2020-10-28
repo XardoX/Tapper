189faec6c4b987677bad10a1fa60518e4a921c82
XardoX <jurek.gab.jg@gmail.com>
2020-10-28 20:29:34 +0100

    started working on input

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour, Controls.IPlayerActions
+{
+    public static Controls controls;
+    private void OnEnable()
+    {
+       if (controls == null)
+        {
+            controls = new Controls();
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnTap(InputAction.CallbackContext context)
+    {
+        Debug.Log("D");
+    }
+}
